53a385adc1a27a3f2838991c86b08837ac3a4748
группы стационара <st@itstep.lan>
2021-03-24 21:52:48 +0300

    24.03.2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shop-website';
+
+  public sliderItems: Array<{name: string, price: number}> = [
+    {
+      name: 't-shirt',
+      price: 5,
+    },
+    {
+      name: 'Pants FORCLAZ',
+      price: 30,
+    },
+    {
+      name: 'backpack',
+      price: 60,
+    },
+    {
+      name: 'hat',
+      price: 15,
+    },
+    {
+      name: 'scarf',
+      price: 12,
+    },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DragScrollModule  } from 'ngx-drag-scroll';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragScrollModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
