ac5d53b9d32cf6c73aae5cb6e9da2dfdbbcceb95
группы стационара <st@itstep.lan>
2021-06-16 19:55:49 +0300

    16.06

--- server.js
+++ server.js
@@ -2,92 +2,27 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/dist/angular-app'));
+app.use(express.static(__dirname + '/dist/shop-website'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/dist/angular-app/index.html");
+  res.sendFile(__dirname + "/dist/shop-website/index.html");
 });
 
 app.get('/api/menSlides', (req, res ) => {
-    res.json([
-        {
-          "id": "1",
-          "name":"T-shirt",
-          "price":30
-        },
-        {
-          "id": "2",
-          "price":10,
-          "name":"Ololo"
-        },
-        {
-          "id": "3",
-          "name":"Test",
-          "price":50
-        },
-        {
-          "id": "4",
-          "name":"Hat",
-          "price":20
-        },
-        {
-          "id": "5",
-          "name":"Pants",
-          "price":70
-        },
-        {
-          "id": "6",
-          "name":"Glasses",
-          "price":170
-        }
-    ]);
+    res.json('/dist/assets/mocks/menSlides.json');
 });
 
 app.get('/api/womenSlides', (req, res ) => {
-  res.json([
-    {
-      "name":"T-shirt",
-      "price":70,
-      "id": "7",
-      "desc": "some text"
-    },
-    {
-      "price":10,
-      "name":"Ololo",
-      "id": "8",
-      "desc": "some text"
-    },
-    {
-      "name":"Test",
-      "price":50,
-      "id": "9",
-      "desc": "some text"
-    },
-    {
-      "name":"Hat",
-      "price":20,
-      "id": "10",
-      "desc": ""
-    },
-    {
-      "name":"Pants",
-      "price":70,
-      "id": "11",
-      "desc": ""
-    },
-    {
-      "name":"Glasses",
-      "price":170,
-      "id": "12",
-      "desc": ""
-    }
-]);
+  res.json('/dist/assets/mocks/womenSlides.json');
+});
+
+app.get('/api/childrenSlides', (req, res ) => {
+  res.json('/dist/assets/mocks/childrenSlides.json');
+});
 
 app.get('*', (req, res)=> {
   res.redirect('/');
 });
-
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
